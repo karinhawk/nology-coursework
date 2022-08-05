62e5ecd32a3f9f57d6405c2472600945148c7b8a
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-08-05 14:55:46 +0100

    wrote out challenges in comments and finishes challenge 1

---
+++ 03-Javascript/javascript_challenges/script.js
@@ -0,0 +1,31 @@
+//Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
+let isNumberPositive = (number) => {
+    if (number > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isNumberPositive(4));
+console.log(isNumberPositive(-12));
+
+//Write a function that takes a number of days and converts it into an age.
+let convertDaysToAge = (days) => {
+//365 days in year so divide by 365
+}
+
+//Write a function that takes three numbers and returns the largest of the three numbers.
+let getLargestNumber = (number1, number2, number3) => {
+
+}
+
+//Write a function that takes an array of names and returns the last name from the array of names.
+let getLastName = () => {
+
+}
+
+//Write a function that takes an array of numbers and returns true if all of the numbers are positive.
+// It should return false if there are one or more negative numbers in the array.
+let allNumbersPositive = () => {
+
+}
